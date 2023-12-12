815481d4a97618377ee904d3ed33052e65274ed1
yinkwokysd <106163305+yinkwokysd@users.noreply.github.com>
2023-12-13 06:38:08 +0800

    add save button

--- frontend/src/components/common/data-edit/index.js
+++ frontend/src/components/common/data-edit/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Metadata from './metadata';
+import { Save } from '@mui/icons-material';
+import { Button } from '@mui/material';
 
 function DataEdit({ context }) {
   return (
     <div className="p-4 border-2 border-zinc-300 shadow-md">
-      <div className="">
+      <div className="flex justify-between">
         <Metadata context={context} />
+        <Button>
+          <Save />
+        </Button>
       </div>
 
       <div className=""></div>
